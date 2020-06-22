20f495beeaf116d65bf54ad2d10b6673b7b5cfc5
Leibniz.Hu <leibnizhu@gmail.com>
2020-06-22 20:32:01 +0800

    feat(): support defaultNull

--- nusadua/src/main/java/io/github/leibnizhu/nusadua/MethodOverloadTranslator.java
+++ nusadua/src/main/java/io/github/leibnizhu/nusadua/MethodOverloadTranslator.java
@@ -1,6 +1,7 @@
 package io.github.leibnizhu.nusadua;
 
 import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
@@ -51,26 +52,30 @@ class MethodOverloadTranslator extends TreeTranslator {
                     Set<String> curMethodSignSet = methodSignMap.computeIfAbsent(methodName, name -> new HashSet<>());
                     curMethodSignSet.add(curMethodSign);
                     messager.printMessage(NOTE, "current Method:" + jcMethod.name + "(" + jcMethod.params + ")");
-                    for (JCTree.JCAnnotation annotation : modifiers.annotations) {
-                        if (!METHOD_OVERLOAD_CLASS_NAME.equals(annotation.getAnnotationType().toString())) {
-                            continue;
-                        }
-                        String errMsg = String.format("%s.%s(%s) method has a MethodOverload annotation's ERROR,Annotation definition: %s: ",
-                                classFullName, methodName, jcMethod.params, annotation);
+                    List<JCTree.JCAnnotation> methodOverloadAnnotationList = List.from(modifiers.annotations.stream()
+                            .filter(annotation -> METHOD_OVERLOAD_CLASS_NAME.equals(annotation.getAnnotationType().toString()))
+                            .collect(Collectors.toList()));
+                    String errMsgTmpl = String.format("%s.%s(%s) method has a MethodOverload annotation's ERROR,Annotation definition: ",
+                            classFullName, methodName, jcMethod.params);
+                    Map<String, Object> defaultValueMap = new HashMap<>();
+                    methodOverloadAnnotationList = validateAndParseAnnotation(jcMethod, methodOverloadAnnotationList, defaultValueMap, errMsgTmpl);
+                    for (JCTree.JCAnnotation annotation : methodOverloadAnnotationList) {
+                        String errMsg = errMsgTmpl + annotation + ", ERROR: ";
                         messager.printMessage(NOTE, "current Annotation:" + annotation.toString() + "," + annotation.getArguments());
-                        JCTree.JCModifiers flagsOnly = treeMaker.Modifiers(modifiers.flags);
                         Pair<String, Object> defaultFieldPair = validateAnnotation(jcMethod.params, annotation, errMsg);
                         if (defaultFieldPair == null) {
                             continue;
                         }
+                        defaultValueMap.put(defaultFieldPair.fst, defaultFieldPair.snd);
                         List<JCTree.JCVariableDecl> newParams = genNewParamList(jcMethod.getParameters(), defaultFieldPair.fst, curMethodSignSet, errMsg);
                         if (newParams == null) {
                             continue;
                         }
-                        List<JCTree.JCStatement> newStatements = genBodyStats(jcMethod.params, jcMethod.name, defaultFieldPair);
+                        List<JCTree.JCStatement> newStatements = genBodyStats(jcMethod, defaultFieldPair);
                         if (newStatements == null) {
                             continue;
                         }
+                        JCTree.JCModifiers flagsOnly = treeMaker.Modifiers(modifiers.flags);
                         JCTree.JCBlock codeBlock = treeMaker.Block(jcMethod.body.flags, newStatements); //TODO 新建一句，调用原方法；返回空则return
                         newMethods.add(treeMaker.MethodDef(flagsOnly, jcMethod.name, jcMethod.restype, jcMethod.getTypeParameters(),
                                 newParams, jcMethod.getThrows(), codeBlock, jcMethod.defaultValue));
@@ -83,11 +88,34 @@ class MethodOverloadTranslator extends TreeTranslator {
         super.visitClassDef(jcClass);
     }
 
+    private List<JCTree.JCAnnotation> validateAndParseAnnotation(JCTree.JCMethodDecl jcMethod, List<JCTree.JCAnnotation> methodOverloadAnnotationList,
+                                                                 Map<String, Object> defaultValueMap, String errMsgTmpl) {
+        List<JCTree.JCAnnotation> result = List.nil();
+        for (JCTree.JCAnnotation annotation : methodOverloadAnnotationList) {
+            String errMsg = errMsgTmpl + annotation + ", ERROR: ";
+            messager.printMessage(NOTE, "current Annotation:" + annotation.toString() + "," + annotation.getArguments());
+            Pair<String, Object> defaultFieldPair = validateAnnotation(jcMethod.params, annotation, errMsg);
+            if (defaultFieldPair != null) {
+                defaultValueMap.put(defaultFieldPair.fst, defaultFieldPair.snd);
+                result = result.append(annotation);
+            }
+        }
+        return result;
+    }
+
     private String calMethodSign(List<JCTree.JCVariableDecl> parameterList) {
         return parameterList.stream()
                 .map(variable -> variable.vartype.toString())
                 .collect(Collectors.joining("_"));
     }
+
+    private static final Map<String, String> TYPE_TO_ANNOTATION_PROPERTY = new HashMap<>();
+
+    static {
+        TYPE_TO_ANNOTATION_PROPERTY.put("integer", "defaultInt");
+        TYPE_TO_ANNOTATION_PROPERTY.put("character", "defaultChar");
+    }
+
 
     /**
      * Validate the annotation,find field to overload, and its default value
@@ -108,7 +136,7 @@ class MethodOverloadTranslator extends TreeTranslator {
         messager.printMessage(NOTE, "parsed Annotation:" + annotationValueMap);
         Object fieldNameObj = annotationValueMap.get("field");
         if (!(fieldNameObj instanceof String)) {
-            messager.printMessage(MANDATORY_WARNING, errMsg + " has no 'field' property! It will be ignore...");
+            messager.printMessage(MANDATORY_WARNING, errMsg + "has no 'field' property! It will be ignore...");
             return null;
         }
         String fieldName = (String) fieldNameObj;
@@ -116,18 +144,24 @@ class MethodOverloadTranslator extends TreeTranslator {
                 .filter(p -> fieldName.equals(p.name.toString())).findFirst();
         if (!fieldParamOpt.isPresent()) {
             messager.printMessage(MANDATORY_WARNING,
-                    String.format(errMsg + " field '%s' is not presented in method parameters! it will be ignore...", fieldName));
+                    String.format(errMsg + "field '%s' is not presented in method parameters! it will be ignore...", fieldName));
             return null;
         }
         JCTree.JCVariableDecl fieldParam = fieldParamOpt.get();
         String fieldType = fieldParam.vartype.toString();
-        String defaultValueKey = "default" + fieldType.substring(0, 1).toUpperCase() + fieldType.substring(1);
+        String defaultValueKey = TYPE_TO_ANNOTATION_PROPERTY.getOrDefault(fieldType.toLowerCase(),
+                "default" + fieldType.substring(0, 1).toUpperCase() + fieldType.substring(1));
         Object defaultValueObj = annotationValueMap.get(defaultValueKey);
         if (defaultValueObj == null) {
-            messager.printMessage(MANDATORY_WARNING, String.format(
-                    errMsg + " field '%s''s default value(property name=%s) doesn't set! It will be ignore...",
-                    fieldName, defaultValueKey));
-            return null;
+            Object defaultNullObj = annotationValueMap.get("defaultNull");
+            if (defaultNullObj instanceof Boolean) {
+                defaultValueObj = null;
+            } else {
+                messager.printMessage(MANDATORY_WARNING, String.format(
+                        errMsg + "field '%s''s default value(property name=%s) doesn't set! It will be ignore...",
+                        fieldName, defaultValueKey));
+                return null;
+            }
         }
 
         return new Pair<>(fieldName, defaultValueObj);
@@ -149,11 +183,32 @@ class MethodOverloadTranslator extends TreeTranslator {
         return newParamList;
     }
 
-    private List<JCTree.JCStatement> genBodyStats(List<JCTree.JCVariableDecl> originParams, Name methodName, Pair<String, Object> defaultFieldPair) {
-        List<JCTree.JCExpression> argTypeList = List.from(originParams.stream().map(v -> v.vartype).collect(Collectors.toList()));
+    private List<JCTree.JCStatement> genBodyStats(JCTree.JCMethodDecl jcMethod, Pair<String, Object> defaultFieldPair) {
+        List<JCTree.JCVariableDecl> originParams = jcMethod.params;
+        Name methodName = jcMethod.name;
+        JCTree.JCExpression returnType = jcMethod.restype;
+
+//        List<JCTree.JCExpression> argTypeList = List.from(originParams.stream().map(v -> v.vartype).collect(Collectors.toList()));
         List<JCTree.JCExpression> argList = List.nil();
-        JCTree.JCExpression callOriginMethod = treeMaker.Apply(argTypeList, memberAccess(methodName.toString()), argList);
-        return List.of(treeMaker.Return(callOriginMethod));
+        for (JCTree.JCVariableDecl originParam : originParams) {
+            String argName = originParam.name.toString();
+            if (defaultFieldPair.fst.equals(argName)) {
+                if (defaultFieldPair.snd == null) {
+                    argList = argList.append(treeMaker.Literal(TypeTag.BOT,null));
+                } else {
+                    argList = argList.append(treeMaker.Literal(defaultFieldPair.snd));
+                }
+            } else {
+                argList = argList.append(memberAccess(argName));
+            }
+        }
+        JCTree.JCExpression callOriginMethod = treeMaker.Apply(List.nil(), memberAccess(methodName.toString()), argList);
+//        JCTree.JCExpression callOriginMethod = treeMaker.App(memberAccess(methodName.toString()), argList);
+        if ("void".equals(returnType.type.toString())) {
+            return List.of(treeMaker.Exec(callOriginMethod));
+        } else {
+            return List.of(treeMaker.Return(callOriginMethod));
+        }
     }
 
     private Name getNameFromString(String s) {
--- nusadua/src/test/java/io/github/leibnizhu/nusadua/NusaduaTest.java
+++ nusadua/src/test/java/io/github/leibnizhu/nusadua/NusaduaTest.java
@@ -2,6 +2,7 @@ package io.github.leibnizhu.nusadua;
 
 
 import io.github.leibnizhu.nusadua.annotation.MethodOverload;
+import org.junit.Test;
 
 import javax.annotation.Generated;
 
@@ -9,6 +10,17 @@ import javax.annotation.Generated;
  * @author Leibniz on 2020/06/19 12:15 AM
  */
 public class NusaduaTest {
+
+    @Test
+    public void primitiveTypesTest() {
+        printPrimitiveTypes("a", (byte) 0, (short) 0, 0, 0, 0, 0, true, 'd');
+        //omit str
+        printPrimitiveTypes((byte) 0, (short) 0, 0, 0, 0, 0, true, 'd');
+        //omit i
+        printPrimitiveTypes("a", (byte) 0, (short) 0, 0, 0, 0, true, 'd');
+        //omit str and i
+//        nusaduaTest.printPrimitiveTypes((byte) 0, (short) 0, 0, 0, 0, true, 'd');
+    }
 
     @Generated("d")
     @MethodOverload(field = "i", defaultInt = -1)
@@ -21,16 +33,23 @@ public class NusaduaTest {
     @Generated("d")
     @MethodOverload(field = "i", defaultInt = -1)
     @MethodOverload(field = "str", defaultString = "hahaha")
-    private void printBoxedTypes(String str, byte b, short s, int i, long l, float f, double d, boolean bool, char c) {
+    private void printBoxedTypes(String str, Byte b, Short s, Integer i, Long l, Float f, Double d, Boolean bool, Character c) {
         System.out.println(String.format("String=%s, byte=%s, short=%s, int=%s, long=%s, float=%s, double=%s, boolean=%s, char=%s",
                 str, b, s, i, l, f, d, bool, c));
+    }
+
+    @Test
+    public void printErrorAnnotationTest(){
+        printErrorAnnotation(new Object(), 233);
+        printErrorAnnotation(233);
+//        printErrorAnnotation(new Object());
     }
 
     @Generated("d")
     @MethodOverload(field = "obj", defaultNull = true)
     @MethodOverload(field = "ddd", defaultNull = true)
     @MethodOverload(field = "i", defaultBool = true)
-    private void print(Object obj, int i) {
+    private void printErrorAnnotation(Object obj, int i) {
         System.out.println(String.format("Object=%s, int=%s", obj, i));
     }
 
@@ -39,10 +58,4 @@ public class NusaduaTest {
     private void print(String str1, String str2) {
         System.out.println(String.format("String1=%s, String2=%s", str1, str2));
     }
-
-    public static void main(String[] args) {
-        NusaduaTest nusaduaTest = new NusaduaTest();
-        nusaduaTest.printPrimitiveTypes("a", (byte) 0, (short) 0, 0, 0, 0, 0, true, 'd');
-    }
-
 }
